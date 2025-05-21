ddf5eaad07dd069c9930a2b530133df6ef16f6de
rudrodip <official.rudrodipsarker@gmail.com>
2025-05-22 01:27:17 +0600

    updated marketing layout

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,7 +1,23 @@
+import Footer from "@/components/landing/footer";
+import HomeNavbar from "@/components/landing/navbar";
+
+
 export default function MarketingLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <main>{children}</main>;
+  return (
+    <div className="w-full">
+      <header className="sticky top-2 z-50 px-2 lg:px-0">
+        <HomeNavbar />
+      </header>
+      <div className="w-full flex-1">
+        {children}
+      </div>
+      <div className="border-t">
+        <Footer />
+      </div>
+    </div>
+  )
 }
