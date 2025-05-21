b7a4231cb09e39baba3d0e672d984b3f4fcc8708
rudrodip <official.rudrodipsarker@gmail.com>
2025-05-22 01:27:09 +0600

    updated

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,12 @@
+import Hero from "@/components/landing/hero";
+import Features from "@/components/landing/features";
+import FAQs from "@/components/landing/faqs";
 export default function Home() {
   return (
-    <div>
-      hi
+    <div className="w-full space-y-10">
+      <Hero />
+      <Features />
+      <FAQs />
     </div>
   );
 }
