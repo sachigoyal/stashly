8d4a9e4ac0af689a2fece97e972dac1194d6ff8c
rudrodip <official.rudrodipsarker@gmail.com>
2025-05-22 14:25:49 +0600

    init

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import * as dotenv from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
-dotenv.config();
+dotenv.config({ path: ".env.local" });
 
 if(!process.env.DATABASE_URL) {
     throw new Error("DATABASE_URL is not defined in .env file");
